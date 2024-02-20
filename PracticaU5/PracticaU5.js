e9cfe605c61b6c46727946af204d014526f5105d
Carlosdelafp <127496890+Carlosdelafp@users.noreply.github.com>
2024-02-20 23:30:01 +0100

    Add files via upload

--- PracticaU5/PracticaU5.js
+++ PracticaU5/PracticaU5.js
@@ -1,1 +1,20 @@
+document.getElementById('btnControls').addEventListener('click', function() {
+    document.getElementById('main-menu').style.display = 'none';
+    document.getElementById('instructions').style.display = 'block';
+});
 
+document.getElementById('btnStart').addEventListener('click', function() {
+    document.getElementById('main-menu').style.display = 'none';
+    document.getElementById('game-container').style.display = 'block';
+    startGame();
+});
+
+document.getElementById('btnBack').addEventListener('click', function() {
+    document.getElementById('instructions').style.display = 'none';
+    document.getElementById('main-menu').style.display = 'block';
+});
+
+function startGame() {
+    // Aquí comienza el juego
+    console.log('Comienza el juego...');
+}
